956b7fc97d49d36d5e701f5acc2772f22494826e
shatter9652 <nathanielswayze2003@gmail.com>
2026-03-17 11:47:41 -0400

    Update KeyboardMouseInput.h

--- Minecraft.Client/Windows64/KeyboardMouseInput.h
+++ Minecraft.Client/Windows64/KeyboardMouseInput.h
@@ -35,6 +35,7 @@ public:
 	static const int KEY_SCREENSHOT = VK_F2;
 	static const int KEY_DEBUG_INFO = VK_F3;
 	static const int KEY_DEBUG_MENU = VK_F4;
+	static const int KEY_THIRD_PERSON = VK_F5;
 	static const int KEY_HOST_SETTINGS = VK_F8;
 	static const int KEY_FULLSCREEN = VK_F11;
 
